22f2dcad7ec877593895af5adf0a08ae919a6eec
pedrogonic <pedrogonic@poli.ufrj.br>
2017-03-12 19:35:47 -0300

    Website first commit.

---
+++ src/java/db/SQLUtils.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author pedrogonic
+ */
+public class SQLUtils {
+    
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static String test () {
+        
+        java.sql.Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String result = "";
+        
+        try {
+            con = getConnection();
+            
+            String query = "SELECT * FROM "+Secret.TEST_TABLE;
+            
+            ps = con.prepareCall(query);
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                result = "ID: "+rs.getInt(1) + " STRING: "+rs.getString(2)+"\n";
+            }
+            
+        } catch(Exception e) {e.printStackTrace();}
+        finally {
+            if (con != null) { try {con.close();}catch(Exception e){}}
+            if (ps != null) { try {ps.close();}catch(Exception e){}}
+            if (rs != null) { try {rs.close();}catch(Exception e){}}
+        }
+        
+        
+        return result;
+    }
+    
+    private static java.sql.Connection getConnection() throws Exception{
+        java.sql.Connection con;
+        try {
+            Class.forName(Secret.DRIVER);
+            con = DriverManager.getConnection(Secret.URL + Secret.DBNAME, Secret.USERNAME, Secret.PASSWORD);
+        }
+        catch (Exception e) {
+                throw e;
+        }
+        return con;
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : Mar 12, 2017, 5:35:35 PM
+    Author     : pedrogonic
+--%>
+
+<%@page import="db.SQLUtils"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1><%=SQLUtils.test()%></h1>
+    </body>
+</html>
